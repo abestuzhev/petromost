b19e8a036feb4db6d6582f3d6e8eff801d7062b5
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2017-12-13 15:56:08 +0300

    changes

---
+++ product-card.js
@@ -0,0 +1,11 @@
+$(function(){
+    /*простые табы*/
+    $(".product-card .tabs-menu a").click(function(event) {
+        event.preventDefault();
+        $(this).parent().addClass("current");
+        $(this).parent().siblings().removeClass("current");
+        var tab = $(this).attr("href");
+        $(".tab-content").not(tab).css("display", "none");
+        $(tab).fadeIn();
+    });
+});
