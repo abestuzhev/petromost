bf21fed90f6014c8529b1853523d93c1812decea
Aleksey Bestuzhev <aleksey.bestuzhev@gmail.com>
2019-01-10 22:07:57 +0300

    changes

--- product-card.js
+++ product-card.js
@@ -34,4 +34,55 @@ $(function(){
     $datepicker.datepicker({
         autoClose: true
     });
+
+    $(document).on('click', '.datepicker-layout__icon', function(e){
+        e.preventDefault();
+        $('.datepicker-custom').trigger('focus');
+    });
+
+
+    $('.js-input-phone').mask('+7(000)000-00-00');
+
+
+    $(document).on('click', '.js-popup-close', function (e) {
+        e.preventDefault();
+        var $html = $('html');
+        $(this).parents('.mfp-wrap').removeClass('is-visible');
+        $('.mfp-bg').removeClass('is-visible');
+        $html.css({
+            'margin-right':'0'
+        }).removeClass('lock-html');
+        $('.wrapper').removeClass('fixed-input');
+        $('.header.sticky').css({
+            // 'right':'0'
+        });
+    });
+
+    var windowWidth = (window.innerWidth ); // вся ширина окна
+    var documentWidth = (document.documentElement.clientWidth );
+    function showPopup(icon, popup) {
+        $(document).on('click', icon, function (e) {
+            var $html = $('html');
+            e.preventDefault();
+            $(popup).addClass('is-visible');
+            $('.mfp-bg').addClass('is-visible');
+
+
+            $html.addClass('lock-html');
+            $('body').addClass('fixed-input');
+            if(windowWidth > documentWidth){
+                $html.css({
+                    'margin-right':'17px'
+                });
+                $('.mfp-wrap').css({
+                    'overflow-y':'scroll'
+                });
+                // console.log('Есть полоса прокрутки');
+            }else {
+                // console.log('Нет полосы прокрутки');
+            }
+        });
+    }
+
+    showPopup(".js-pie-popup", '.popup-pie-order');
 });
